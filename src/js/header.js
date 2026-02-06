86d61582b83eef13387ad71da12f46d89325d6ad
Miyazmm <alinakulichenko1@gmail.com>
2026-02-06 18:09:07 +0200

    header-mobile-menu-added

--- src/js/header.js
+++ src/js/header.js
@@ -1,3 +1,15 @@
-const openMenuBtn = document.querySelector(".burger-icon")
-const closeMenuBtn = document.querySelector(".close-icon")
-const navMenu = document.querySelector(".modal-menu")
+const openMenuBtn = document.querySelector(".burger-icon-btn")
+const closeMenuBtn = document.querySelector(".close-icon-btn")
+const navMenu = document.getElementById("menuModal")
+
+openMenuBtn.addEventListener("click", () => {
+    openMenuBtn.style.display = "none"
+    closeMenuBtn.style.display = "flex"
+    navMenu.style.display = "flex"
+})
+
+closeMenuBtn.addEventListener("click", () => {
+    openMenuBtn.style.display = "flex"
+    closeMenuBtn.style.display = "none"
+    navMenu.style.display = "none"
+})
