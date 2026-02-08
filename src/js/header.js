b1403ca535ba4c45ff88be3570f831dcf99d98ad
Miyazmm <alinakulichenko1@gmail.com>
2026-02-09 01:28:50 +0200

    adding-header-markup-through-js

--- src/js/header.js
+++ src/js/header.js
@@ -1,15 +1,80 @@
+const header = document.querySelector(".header")
+
+// markup
+header.innerHTML = `
+<div class="container">
+
+<nav class="page-nav">
+      <a href="./" class="logo"
+        ><svg class="icon logo">
+          <use href="../img/sprite.svg#logo-2"></use>
+        </svg>
+      </a>
+      <button type="button" class="burger-icon-btn" id="burger-icon-btn">
+        <svg class="icon burger-icon">
+          <use href="../img/sprite.svg#burger"></use>
+        </svg>
+      </button>
+      <button type="button" class="close-icon-btn" id="close-icon-btn">
+          <svg class="icon close-icon">
+            <use href="../img/sprite.svg#close-x"></use>
+          </svg>
+        </button>
+
+      <ul class="section-nav">
+        <li class="section-name">
+          <a href="#artists" class="section-link">Artists</a>
+        </li>
+        <li class="section-name">
+          <a href="#about-us" class="section-link">About Us</a>
+        </li>
+        <li class="section-name">
+          <a href="#feedback" class="section-link">Reviews</a>
+        </li>
+      </ul>
+    </nav>
+    </div>
+
+    <div class="menu-modal">
+    <div class="container">
+      <ul class="mobile-section-nav">
+        <li class="mobile-section-name">
+          <a href="#artists" class="mobile-section-link">Artists</a>
+        </li>
+        <li class="section-name">
+          <a href="#about-us" class="mobile-section-link">About Us</a>
+        </li>
+        <li class="section-name">
+          <a href="#feedback" class="mobile-section-link">Reviews</a>
+        </li>
+      </ul>
+      </div>
+
+      </div>`;
+
+
 const openMenuBtn = document.querySelector(".burger-icon-btn")
 const closeMenuBtn = document.querySelector(".close-icon-btn")
-const navMenu = document.getElementById("menuModal")
+const menuModal = document.querySelector(".menu-modal")
+const mobileNav = document.querySelectorAll(".mobile-section-link")
 
+
+// open Menu
 openMenuBtn.addEventListener("click", () => {
-    openMenuBtn.style.display = "none"
-    closeMenuBtn.style.display = "flex"
-    navMenu.style.display = "flex"
+    header.classList.add("menu-open")
+    menuModal.classList.add("is-open")
 })
 
+// close Menu
 closeMenuBtn.addEventListener("click", () => {
-    openMenuBtn.style.display = "flex"
-    closeMenuBtn.style.display = "none"
-    navMenu.style.display = "none"
+    header.classList.remove("menu-open")
+    menuModal.classList.remove("is-open")
+})
+
+// close Menu when clicking on internal links
+mobileNav.forEach(link => {
+    link.addEventListener("click", () => {
+        header.classList.remove("menu-open")
+        menuModal.classList.remove("is-open")
+    })
 })
